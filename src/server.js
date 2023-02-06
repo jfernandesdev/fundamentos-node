f0c215626924c14aa56b5f2c7001fa301f1728e4
jfernandesdev <jfernandes.dev@gmail.com>
2023-02-06 13:49:46 -0300

    http methods, headers and status code

--- src/server.js
+++ src/server.js
@@ -1,7 +1,28 @@
-import http from 'node:http'
+import http from 'node:http';
+
+//stateful
+const users = [];
 
 const server = http.createServer((req, res) => {
-  return res.end('Hello World!')
+  const { method, url } = req;
+
+  if(method === 'GET' && url === '/users') {
+    return res
+    .setHeader('Content-type', 'application/json')
+    .end(JSON.stringify(users));
+  }
+
+  if(method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: 'Jeferson Fernandes',
+      email: 'jfernandes.dev@gmail.com',
+    })
+
+    return res.writeHead(201).end();
+  }
+
+  return res.writeHead(404).end()
 })
 
 server.listen(3333)
